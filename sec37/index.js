61193e26e46bddc3443a1ca280fc38abd2d7b645
Joseph <josephiusx@gmail.com>
2021-04-13 14:33:57 -0700

    L380. Our first mongoose Model

--- sec37/index.js
+++ sec37/index.js
@@ -7,3 +7,18 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, u
     console.log("OH NO ERROR !!!");
     console.log(err);
 })
+
+// Our first mongoose model
+
+// schemas
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+// take the schema and tell mongoose to make a model using the schema
+const Movie = mongoose.model('Movie', movieSchema) // pass in name of model and the schema, Movie createw a collection 'movies' in mongoose , save it to a variable and save it to a variable Movie
+const amadeus = new Movie ({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'}) // this is our js object that we can change and save to the db
+// as of this point nothing has been created in my mongodb movies database
