b15db50a6f01e04d667ba6a8afafbec0ec9d84f6
Joseph <josephiusx@gmail.com>
2021-05-24 17:33:09 -0700

    went over lesson 380

--- sec37/index.js
+++ sec37/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); // require mongoose after installing it on npm
-mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true}) // where to find mongodb locally / database (if it dosent exist one will be created)
+mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true}) // default mongo port, copy from  where to find mongodb locally / database (if it dosent exist one will be created)
 .then(() => { // try
     console.log("CONNECTION OPEN!!!")
 })
@@ -23,17 +23,17 @@ const Movie = mongoose.model('Movie', movieSchema); // pass in name of model and
 // const amadeus = new Movie ({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'}) // this is our js object that we can change and save to the db
 // as of this point nothing has been created in my mongodb movies database until we open the file with node and save it
 
-// const blah = new Movie(); // normally
-// blah.save()
+const blah = new Movie(); // normally
+blah.save()
 
-// Movie.insertMany([ // with this we dont need to call a save method
-//     {title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
-//     {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
-//     {title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
-//     {title: 'Stand By Me', year:1986, score: 8.6, rating: 'R'},
-//     {title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
-// ])
-// .then(data => {
-//     console.log('It WORKED!')
-//     console.log(data);
-// })
+Movie.insertMany([ // with this we dont need to call a save method
+    {title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+    {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+    {title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+    {title: 'Stand By Me', year:1986, score: 8.6, rating: 'R'},
+    {title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+])
+.then(data => {
+    console.log('It WORKED!')
+    console.log(data);
+})
