9e1cc969f734c83a2be6e15629400a509274b3f1
Joseph <josephiusx@gmail.com>
2021-06-14 20:36:15 -0700

    asked a question

---
+++ sec49/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate'); // require after npm install
+const ExpressError = require('./utils/ExpressError')
+const methodOverride = require('method-override');
+
+
+const campgrounds = require('./routes/campgrounds');
+const Review = require('./routes/reviews');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    userNewUrlParser: true,
+    userCreateIndx: true,
+    useUnifiedTopology: true
+}); 
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const app  = express();
+
+app.engine('ejs', ejsMate); // tell express to use ejsMate
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.all('*', (req, res, next) =>{ // every request, will only run if nothing else is matched first , thus is best at the end 
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => { // basic error route
+    const {statusCode = 500, message = 'something went wrong'} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+});
---
+++ sec49/models/campground.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [ // referencint the review model
+        {
+            type: Schema.Types.ObjectId, // object id
+            ref: 'Review' // from thes model
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({ // deletes the reviews from db when 
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
