f5ccdc7d8f2038589ebb16153d6c7ad00a7c0708
Joseph <josephiusx@gmail.com>
2021-06-19 20:15:35 -0700

    511. currentUser Helper

--- sec51/middleware.js
+++ sec51/middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.flash('error', 'you must be signed in first!');
-        return res.redirect('/login')
+        return res.redirect('/login');
     }
     next();
 }
