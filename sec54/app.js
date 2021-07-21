4b8618600a62c7e412b017ee665d8df76058a05b
Joseph <josephiusx@gmail.com>
2021-07-21 15:10:11 -0700

    added dotenv

--- sec54/app.js
+++ sec54/app.js
@@ -21,8 +21,6 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
