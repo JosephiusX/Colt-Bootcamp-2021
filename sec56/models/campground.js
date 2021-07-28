bd78a6dfcb9b8e788a2c5899a8e9c293843e4936
Joseph <josephiusx@gmail.com>
2021-07-27 17:05:35 -0700

    554. Styling Home Page

--- sec56/models/campground.js
+++ sec56/models/campground.js
@@ -1,55 +1,66 @@
-const mongoose = require('mongoose');
-const Review = require('./review')
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-        url: String,
-        filename: String 
-})
+  url: String,
+  filename: String,
+});
 
-ImageSchema.virtual('thumbnail').get(function() {
-    return this.url.replace('/upload', '/upload/w_200');
-})
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 
-const CampgroundSchema = new Schema({
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
     title: String,
-    images:[ImageSchema],
+    images: [ImageSchema],
     geometry: {
-        type : {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates:{
-            type: [Number],
-            required: true  
-        }
-},
-price: Number,
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    reviews: [ // referencint the review model
-        {
-            type: Schema.Types.ObjectId, // object id
-            ref: 'Review' // from thes model
-        }
-    ]
+    reviews: [
+      // referencint the review model
+      {
+        type: Schema.Types.ObjectId, // object id
+        ref: "Review", // from thes model
+      },
+    ],
+  },
+  opts
+);
+
+// this virtual property includes this markup in every popup
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="campgrounds/${this._id}">${this.title}</a><strong>
+  <p>${this.description.substring(0, 30)}...</p>`;
 });
 
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      // deletes the reviews from db when
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if(doc){
-        await Review.deleteMany({ // deletes the reviews from db when 
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
-
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
